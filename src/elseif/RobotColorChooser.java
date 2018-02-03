8f70e0e5d27bbc0d42ced8236da484fc0fc9ebea
mjfre <theleague@matts-mbp.attlocal.net>
2018-02-03 13:18:28 -0800

    updates

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -9,7 +9,8 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
+		//1.  Create a new Robot
+		
 		//3. ask the user what color they would like the robot to draw
 		
 		//4. use an if/else statement to set the pen color that the user requested
@@ -18,9 +19,9 @@ public class RobotColorChooser {
 
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
-		//2. set the pen width to 10
+		//4. set the pen width to 10
 		
-	    //1. make the robot draw a shape (this will take more than one line of code)
+	    //2. make the robot draw a shape (this will take more than one line of code)
 
 
 	}
